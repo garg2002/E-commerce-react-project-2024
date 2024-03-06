ed7b2bc307a8004c5ca7968b74ed4fcacff34f85
Satyam Garg <satyamgarg1308@gmail.com>
2024-03-06 21:11:40 +0530

    Select Category component

--- src/components/selectDropDown/select.js
+++ src/components/selectDropDown/select.js
@@ -4,7 +4,23 @@ import './select.css'
 const select = () => {
   return (
     <div className='selectDrop-Down'>
-      
+      <div className="searchField">
+        <input type="text" />
+      </div>
+      <ul className='searchResults'>
+          <li>Milk amd Dairies</li>
+          <li>Wines & Drinks</li>
+          <li>Clothing & Beauty</li>
+          <li>Fresh Seafood</li>
+          <li>Pet Foods & Toy</li>
+          <li>Fast Food</li>
+          <li>Baking Material</li>
+          <li>Vegetables</li>
+          <li>Fresh Fruit</li>
+          <li>Bread and Juice</li>
+          <li>Milks and Dairies</li>
+          <li>Wines & Drinks</li>
+        </ul>
     </div>
   )
 }
